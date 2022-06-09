878e5abf5703bc375dcff5a12923a799b2decc57
mika <mika.ct123@mgail.com>
2022-06-09 15:24:10 +0400

    Adding SDL to Circular Version

--- CircularVersion/main.c
+++ CircularVersion/main.c
@@ -1,20 +1,51 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "ansidraw.h"
+#include "drawSDL.h"
 
+/// Circular Implementation of Game of Life
+/// ANSI console or SDL draw board
 
 int main(){
 
-    CellList* cl = CL_newCellList(10,10); 
+    printf("Do you want ANSI console version or SDL version? 'A' for ANSI | 'S' for SDL:  ");
+    char c;
+    scanf("%c",&c);
 
-    while(true){
-        CL_show(cl);
-        CL_implement(cl);
-        if(CL_equalLists(cl)){break;}
-        CL_copy(cl);
+    if( c == 'A'){
+        /// ansi code
         printf("\n");
-        sleep(1);
-    }  
+        CellList* cl = CL_newCellList(10,10);
+
+        while(true){
+            CL_show(cl);
+            CL_implement(cl);
+            if(CL_equalLists(cl)){break;}
+            CL_copy(cl);
+            printf("\n");
+            sleep(1);
+        }  
+    }else if (c == 'S'){
+        /// SDL code
+        drawSDL();
+    }else{
+        printf("Please make correct input next time!\n");
+        return 0;
+    }
+
+    draw(); /// Drawing the board with SDL library
+    
+    // CellList* cl = CL_newCellList(10,10);
+
+    
+    // while(true){
+    //     CL_show(cl);
+    //     CL_implement(cl);
+    //     if(CL_equalLists(cl)){break;}
+    //     CL_copy(cl);
+    //     printf("\n");
+    //     sleep(1);
+    // }
 
     return 0;
 }
